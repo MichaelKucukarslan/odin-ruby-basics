7e2e5c0847669b9cf1f65810f81efef8b164a02e
Michael Aslan <MichaelKucukarslan@gmail.com>
2024-07-30 17:50:37 -0700

    Testing vim in cmder redering error

--- substrings/main.rb
+++ substrings/main.rb
@@ -9,3 +9,4 @@ array_to_test = ["below","down","go","going","horn","how","howdy","it","i","low"
 puts "We are now going to check your words (#{words_to_count}) to the computer provided array [#{array_to_test}]"
 
 puts sub_string_count(words_to_count, array_to_test)
+
